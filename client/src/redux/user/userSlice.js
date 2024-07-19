f7400c5033fd772aee1ae427cc62d9bceffb8013
SyedRohanRafi365 <156239733+SyedRohanRafi365@users.noreply.github.com>
2024-07-19 00:20:56 -0400

    complete update user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,14 +22,31 @@ const userSlice = createSlice({
     signInFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
+
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
     },
   }
 });  
 
-    export const {
-        signInStart,
-        signInSuccess,
-        signInFailure,
-      } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+
+} = userSlice.actions;
       
       export default userSlice.reducer;
