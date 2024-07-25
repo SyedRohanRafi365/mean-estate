c23534e6d0921e43c64b86bebae34f33ab090457
SyedRohanRafi365 <156239733+SyedRohanRafi365@users.noreply.github.com>
2024-07-25 01:36:49 -0400

    added pre installation for live web

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
+//done
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-import listingRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -16,8 +18,9 @@ mongoose
     console.log(err);
   });
 
-const app = express(); 
+  const __dirname = path.resolve();
 
+const app = express();
 
 app.use(express.json());
 
@@ -30,6 +33,13 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
